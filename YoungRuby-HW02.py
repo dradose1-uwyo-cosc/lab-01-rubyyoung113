c98233207cfbe20cbb03249fe0d55b0ddf292bd1
rubyyoung113 <ryoung19@uwyo.edu>
2024-10-29 16:13:01 +0000

    done

--- YoungRuby-HW02.py
+++ YoungRuby-HW02.py
@@ -3,19 +3,7 @@
 # Submission Date: 10/29/24
 # HW 02
 # Lab Section: 16
-# Sources, people worked with, help given to: Nile Young 
-
-# For this homework assignment you will be writing a program that translates between plaintext and Morse Code.
-
-# When your program first starts it should ask the user for the input string. If plaintext alphabet cahracters is entered output the Morse Code equivalent.
-
-#You may assume that only alphabet characters will be entered, and may ignore other input characters.
-
-#You can use the equivalencies below.
-
-
-
-
+# Sources, people worked with, help given to: Nile Young, Isabella 
 
 morse_dict = { 
 'A': '.-',      'N': '-.',
@@ -35,14 +23,12 @@ morse_dict = {
 
 question = input("Input a string: ")
 
-
 my_string = ""
 
 for character in question:
     if character == ' ':
-        my_string += '  '
+        my_string += ' '
     elif character.isalpha():
         my_string += morse_dict[character.upper()] + ' '
-
 
 print(my_string)
